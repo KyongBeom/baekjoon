b49c25645b3a31c049ef6598a4ee9fc556da38d2
KyongBeom <122436476+KyongBeom@users.noreply.github.com>
2023-03-14 13:49:47 +0900

    [Bronze II] Title: OX퀴즈, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,12 @@
+T = int(input())
+for tc in range(T):
+    arr = input()
+    cnt = 0
+    res = 0
+    for i in arr:
+        if i =='O':
+            cnt += 1
+            res += cnt
+        else:
+            cnt = 0
+    print(res)
