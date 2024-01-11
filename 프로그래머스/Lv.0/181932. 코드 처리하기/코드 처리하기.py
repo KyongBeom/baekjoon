6642dd1c818db313c7c5232d103d0981b6a2c6e6
KyongBeom <122436476+KyongBeom@users.noreply.github.com>
2024-01-11 20:26:49 +0900

    [level Lv.0] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,20 @@
+def solution(code):
+    answer = ''
+    mode = 0
+    for i in range(len(code)):
+        if code[i] == "1":
+            if mode == 1:
+                mode = 0
+            else:
+                mode = 1
+        else:
+            if mode == 0:
+                if i%2 == 0:
+                    answer += code[i]
+            else:
+                if i %2:
+                    answer += code[i]
+    if answer:
+        return answer
+    else:
+        return "EMPTY"
