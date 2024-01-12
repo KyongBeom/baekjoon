11a08601c6ae6b9606680a8df7c2a82484f910c7
KyongBeom <122436476+KyongBeom@users.noreply.github.com>
2024-01-12 20:13:37 +0900

    [level Lv.1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/Lv.1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,27 +1,12 @@
 def solution(data, ext, val_ext, sort_by):
-    answer = []
-    for idx in range(len(data)):
-        if ext == "code":
-            if data[idx][0] < val_ext:
-                answer.append(data[idx])
-        elif ext == "date":
-            if data[idx][1] < val_ext:
-                answer.append(data[idx])
-        elif ext == "maximum":
-            if data[idx][2] < val_ext:
-                answer.append(data[idx])
-        else:
-            if data[idx][3] < val_ext:
-                answer.append(data[idx])
-                 
-                    
-    if sort_by == "code":
-        answer.sort(key=lambda answer: answer[0])
-    elif sort_by == "date":
-        answer.sort(key=lambda answer: answer[1])
-    elif sort_by == "maximum":
-        answer.sort(key=lambda answer: answer[2])
-    else:
-        answer.sort(key=lambda answer: answer[3])
+    list = ["code", "date", "maximum", "remain"]
+    ext_idx = list.index(ext)
+    sort_idx = list.index(sort_by)
 
+    answer = []
+    for i in range(len(data)):
+        if data[i][ext_idx] < val_ext:
+            answer.append(data[i])
+
+    answer.sort(key = lambda x: x[sort_idx])
     return answer
