b85764ea4bab18184733b9d0a3aad3fd1e6442b0
KyongBeom <122436476+KyongBeom@users.noreply.github.com>
2024-03-06 14:24:49 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
@@ -0,0 +1,10 @@
+def solution(a, b):
+    if a % 2 and b % 2:
+        return a**2 + b**2
+    elif a% 2 ==0 and b%2 == 0:
+        if a>= b:
+            return a-b
+        else:
+            return b-a
+    else:
+        return 2 * (a + b)
