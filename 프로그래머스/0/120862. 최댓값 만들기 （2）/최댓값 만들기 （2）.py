b00a9d8b2c8ac1c89134e80cd6243cda86aba120
KyongBeom <122436476+KyongBeom@users.noreply.github.com>
2024-03-29 10:58:21 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    answer = 0
+    numbers=sorted(numbers)
+    if numbers[0] * numbers[1] > numbers[-1] * numbers[-2]:
+        return numbers[0] * numbers[1]
+    return numbers[-1] * numbers[-2]
