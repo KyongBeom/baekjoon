d4052f6434a39991385de2c0d31a426098518f8a
KyongBeom <122436476+KyongBeom@users.noreply.github.com>
2025-01-06 10:33:48 +0900

    [Silver I] Title: IOIOI, Time: 336 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,13 +1,21 @@
-n = int(input())
-m = int(input())
-arr = input()
-p = "IOI"
-cnt = 0
-if n >= 2:
-    for i in range(n-1):
-        p += "OI"
+N = int(input())
+M = int(input())
+S = input()
 
-for i in range(m - 3 + 2 * n):
-    if arr[i:i+len(p)] == p:
-        cnt += 1
-print(cnt)
+word = "IOI"
+start = 0
+ans = 0
+check = 0
+
+
+while start < (M-1):
+    if S[start:start+3] == word:
+        start += 2
+        check += 1
+        if check == N:
+            ans += 1
+            check -= 1
+    else:
+        start += 1
+        check = 0
+print(ans)
